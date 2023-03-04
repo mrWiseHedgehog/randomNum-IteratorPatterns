4e7acb4967565ee351c9f044f2751a4ae1985356
Leonid Sitnikov <hedgehog@yandex.ru>
2023-03-04 22:24:25 +0300

    added README.md

---
+++ README.md
@@ -0,0 +1,5 @@
+<h1>randomNum-IteratorPatterns</h1>
+
+==randomNum== - creator randoms
+
+This repository is implements pattern ==Iterator==
---
+++ src/main/java/ru/netology/Randoms.java
@@ -0,0 +1,6 @@
+package ru.netology;
+
+public class Randoms implements Iterable<? extends int> {
+    public Randoms(int i, int i1) {
+    }
+}
